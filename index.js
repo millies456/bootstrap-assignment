8586571842a5468875083267a94990aa42d2b773
millies456 <75187791+millies456@users.noreply.github.com>
2022-10-09 22:27:46 -0400

    s
    d

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-const http = require('http') // IMPORT express
+const http = require('http');
+const fs = require('fs');
 
-const port = 3000 // variable to store the port to listen on
-const server = http.createServer(function(req, res){
-res.write('hello node')
-res.end()
+http.createServer(function(req,res) {
+  res.writeHead(200, { 'content-type' : 'text/html'});
+  const html =fs.readFileSync('./bootstrap.html');
+  res.end(html);
+}).listen(2000,() => {
+  console.log('running on 3000')
 })
-
-server.listen(port ,function(error){
-if(error){
-    console.log('something went wrong', error)
-}else{
-    console.log('server is listening ' + port)
-}
-}
-)
